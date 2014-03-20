147c9a051cc0ba35b557ec764a2a0b888422af5f
micfloy <c15michael.bentley@usafa.edu>
2014-03-20 14:26:22 -0600

    Instantiated PicoBlaze in my top level file.  Still working on connecting the correct signals through the PicoBlaze.  At this point, I have no more functionality than before, but getting closer to having something.  I have yet to start writing my assembly code.

--- atlys_remote_terminal_pb.vhd
+++ atlys_remote_terminal_pb.vhd
@@ -78,11 +78,60 @@ architecture Behavioral of atlys_remote_terminal_pb is
 		);
 	END COMPONENT;
 	
-	signal data : std_logic_vector(7 downto 0);
+	component kcpsm6
+	generic( hwbuild : std_logic_vector(7 downto 0) := X"00";
+		interrupt_vector : std_logic_vector(11 downto 0) := X"3FF";
+		scratch_pad_memory_size : integer := 64);
+	port ( address : out std_logic_vector(11 downto 0);
+		instruction : in std_logic_vector(17 downto 0);
+		bram_enable : out std_logic;
+		in_port : in std_logic_vector(7 downto 0);
+		out_port : out std_logic_vector(7 downto 0);
+		port_id : out std_logic_vector(7 downto 0);
+		write_strobe : out std_logic;
+		k_write_strobe : out std_logic;
+		read_strobe : out std_logic;
+		interrupt : in std_logic;
+		interrupt_ack : out std_logic;
+		sleep : in std_logic;
+		reset : in std_logic;
+		clk : in std_logic);
+	end component;
+	
+	COMPONENT lab4PicoBlaze
+	generic( 				C_FAMILY : string  := "S6";
+					C_RAM_SIZE_KWORDS : integer := 1;
+				C_JTAG_LOADER_ENABLE : integer := 0);
+	PORT(
+		address : IN std_logic_vector(11 downto 0);
+		enable : IN std_logic;
+		clk : IN std_logic;          
+		instruction : OUT std_logic_vector(17 downto 0);
+		rdl : OUT std_logic
+		);
+	END COMPONENT;
+	
+	signal data_in_pico, data_out_pico : std_logic_vector(7 downto 0);
 	signal en_sig, buffer_write_sig, buffer_data_sig : std_logic; 
 	signal half_full_sig, full_sig, buffer_reset_sig : std_logic;
 	
+	signal address : std_logic_vector(11 downto 0);
+	signal instruction : std_logic_vector(17 downto 0);
+	signal bram_enable : std_logic;
+	signal in_port : std_logic_vector(7 downto 0);
+	signal out_port : std_logic_vector(7 downto 0);
+	Signal port_id : std_logic_vector(7 downto 0);
+	Signal write_strobe : std_logic;
+	Signal k_write_strobe : std_logic;
+	Signal read_strobe : std_logic;
+	Signal interrupt : std_logic;
+	Signal interrupt_ack : std_logic;
+	Signal kcpsm6_sleep : std_logic;
+	Signal kcpsm6_reset : std_logic;
+	
 begin
+
+	
 
 				 
 	Inst_clk_to_baud: clk_to_baud PORT MAP(
@@ -92,7 +141,7 @@ begin
 			);
 
 	Inst_uart_tx6: uart_tx6 PORT MAP(
-			data_in => data,
+			data_in => data_out_pico,
 			en_16_x_baud => en_sig,
 			serial_out => serial_out,
 			buffer_write => buffer_write_sig,
@@ -106,7 +155,7 @@ begin
 	Inst_uart_rx6: uart_rx6 PORT MAP(
 			serial_in => serial_in,
 			en_16_x_baud => en_sig,
-			data_out => data,
+			data_out => data_in_pico,
 			buffer_read => buffer_data_sig,
 			buffer_data_present => buffer_write_sig,
 			buffer_half_full => half_full_sig,
@@ -114,7 +163,47 @@ begin
 			buffer_reset => buffer_reset_sig,
 			clk => clk
 		);
+		
+	processor: kcpsm6
+		GENERIC MAP( hwbuild => X"00",
+			interrupt_vector => X"3FF",
+			scratch_pad_memory_size => 64)
+		PORT MAP( address => address,
+			instruction => instruction,
+			bram_enable => bram_enable,
+			port_id => port_id,
+			write_strobe => write_strobe,
+			k_write_strobe => k_write_strobe,
+			out_port => out_port,
+			read_strobe => read_strobe,
+			in_port => in_port,
+			interrupt => interrupt,
+			interrupt_ack => interrupt_ack,
+			sleep => kcpsm6_sleep,
+			reset => kcpsm6_reset,
+			clk => clk );
 
+	Inst_lab4PicoBlaze: lab4PicoBlaze 
+		generic map( C_FAMILY => "S6",
+			C_RAM_SIZE_KWORDS => 1,
+			C_JTAG_LOADER_ENABLE => 1)
+		port map( address => address,
+			instruction => instruction,
+			enable => bram_enable,
+			rdl => kcpsm6_reset,
+			clk => clk);
+			
+	process(clk, reset)
+	begin
+		if(rising_edge(clk)) then
+			case port_id
+				when x"AF" =>
+					port_in <= data_in_pico;
+				when x"0A" =>
+					port_out <= data_out_pico;
+			end case;
+		end if;
+	end process;
 
 
 
